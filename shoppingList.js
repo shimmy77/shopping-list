893a9ae196fc683e0d534cbf5d8265437dcbfb96
shimmy77 <christianshim@gmail.com>
2020-01-08 13:59:41 -0600

    added js file

---
+++ shoppingList.js
@@ -0,0 +1,40 @@
+function createList(value){
+    return `<li><span class="shopping-item">${value}</span><div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+    <span class="button-label">check</span>
+    </button><button class="shopping-item-delete">
+    <span class="button-label">delete</span>
+    </button></div></li>`
+}
+
+function checkedItem(){
+	$('.shopping-list').on('click', '.shopping-item-toggle', function(e){
+		e.preventDefault();
+		$(e.target).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+
+	})
+}
+
+function deleteItem(){
+	$('.shopping-list').on('click', '.shopping-item-delete', function(e){
+		e.preventDefault();
+		$(e.target).closest('li').remove();
+	})
+}
+
+function createShoppingList() {
+	$('#js-shopping-list-form').on('submit', function(e){
+		e.preventDefault();
+
+		const value = $('#shopping-list-entry').val();
+		console.log(value);
+
+		$('.shopping-list').append($(createList(value)));
+		$('#shopping-list-entry').val('');
+	});
+};
+
+
+$(createShoppingList)
+$(checkedItem)
+$(deleteItem)
